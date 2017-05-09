d9f67b1d6d5238848c49cafbd3e908b2907650fa
edson <nos18de@gmail.com>
2017-05-09 00:07:41 -0400

    intefaz curso _ routes

--- client/main.js
+++ client/main.js
@@ -6,3 +6,13 @@
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+Template.main.onRendered(function(){
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+});
+
+Template.cursos.onRendered(function(){
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+});
