37875815a3b75882af8c154b18c9d11306f116ee
edson <nos18de@gmail.com>
2017-06-13 17:59:09 -0400

    solo ver

--- client/main.js
+++ client/main.js
@@ -11,6 +11,9 @@
 Template.main.helpers({
   username : function(){
     return Accounts.user().username;
+  },
+  elcurso: function(){
+    return Curso.find();
   }
 })
 
@@ -44,4 +47,19 @@ Template.crear.onRendered(function(){
 Template.formcurso.onRendered(function(){
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+});
+Template.formcurso.events({
+  "submit form": function(e){
+    var datos = {
+      titulo: e.target.titulo.value,
+      descripcion: e.target.descripcion.value,
+      fecha: e.target.fecha.value
+    };
+    Curso.insert(datos);
+    e.target.titulo.value = "";
+    e.target.descripcion.value = "";
+    e.target.fecha.value = "";
+    return false;
+
+  }
 });
