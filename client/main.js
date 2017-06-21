11f4a62d7f13be21d46b51a7da892c22563757e5
edson <nos18de@gmail.com>
2017-06-21 15:43:44 -0400

    subscribe

--- client/main.js
+++ client/main.js
@@ -51,21 +51,5 @@ Template.formcurso.onRendered(function(){
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
 });
-Template.formcurso.events({
-  "submit form": function(e){
-    var datos = {
-      titulo: e.target.titulo.value,
-      descripcion: e.target.descripcion.value,
-      fecha: e.target.fecha.value
-    };
-    Meteor.call("insert",datos, function(err,result){
-    });
-    //Curso.insert(datos);
-    //e.target.titulo.value = "";
-    //e.target.descripcion.value = "";
-    //e.target.fecha.value = "";
-    //return false;
 
-  }
-});
 
