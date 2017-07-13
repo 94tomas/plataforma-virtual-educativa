eb1e262fd9ae318a30e4e975a60ad264208331ec
edson <nos18de@gmail.com>
2017-07-13 17:42:21 -0400

    pathcorreccion

--- database/collections.js
+++ database/collections.js
@@ -21,8 +21,8 @@ Curso.attachSchema(CursoSchema);
 imgCurso = new FilesCollection({
     collectionName: 'imgCurso',
     allowClientCode: false,
-    storagePath: '/home/luis/luis/algo/images',
-    downloadRoute: '/home/luis/luis/algo/images/download',
+    storagePath: '/home/arandopuma/edson/seminario/SEMINARIO/file/imgCurso',
+    downloadRoute: '/home/arandopuma/edson/seminario/SEMINARIO/file/download',
     permissions: 0755,
     cacheControl: 'public, max-age=3153600'
 });
